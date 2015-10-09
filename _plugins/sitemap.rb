5444c833fa9f26db86f624e13895f4cc6600e1be
Vladimir Chervanev <vlad.chervanev@cloudcastlegroup.com>
2015-10-09 14:10:01 +0300

    Create sitemap.rb

---
+++ _plugins/sitemap.rb
@@ -0,0 +1,33 @@
+module Jekyll
+	class Sitemap < Generator
+		safe true
+		priority :lowest
+		
+		def generate(site)
+			pages = site.pages
+	        languages = site.config['languages'] || ['en']
+	        default_lang = site.config['default_lang'] || 'en'
+
+	        sitemap = {}
+	        languages.each do |lang|
+	            lang == default_lang ? lang = '' : lang = '/' + lang
+	            pages.each do |page|
+	                if sitemap.key?(page.name)
+	                    sitemap[page.name] << lang + page.url
+	          		else
+	            		sitemap.store(page.name, [ lang + page.url ])
+	          		end
+	        	end
+	      	end
+      		save sitemap
+      	end
+
+	    private
+
+	    def save(sitemap)
+	        File.open("sitemap.json", "w") do |f|
+	          f.write(sitemap.to_json)
+	        end
+	    end
+	end
+end
