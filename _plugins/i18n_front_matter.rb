f2a3d418d5c23a5f3d0af9c3a58cbdc0030ed43b
tt-admin <tt-admin@users.noreply.github.com>
2017-04-27 07:58:06 -0400

    Update Plugins

--- _plugins/i18n_front_matter.rb
+++ _plugins/i18n_front_matter.rb
@@ -1,8 +1,12 @@
-Jekyll::Hooks.register :site, :post_read do |site|
-  locale = site.active_lang
-  default_locale = site.default_lang
+Jekyll::Hooks.register :pages, :post_init do |page|
+  locale = page.site.active_lang
+  default_locale = page.site.default_lang
 
-  site.pages.each do |page|
-    page.data = translate_data(page.data, locale, default_locale)
+  page.data = translate_data(page.data, locale, default_locale)
+  permalink = page.data['permalink']
+  if permalink && !(permalink.end_with?('/') || permalink.end_with?('.html'))
+    p "Permalink #{permalink} is invalid. Must ends with '/' or html extension"
+    page.data['permalink'] += '.html'
   end
+
 end
